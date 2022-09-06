f56d5560f026b7322f3025c845ce3bd9d7c8a20d
lily <905355741@qq.com>
2022-09-06 18:12:30 +0800

    feat:login登录页完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 引入
+// import Login from '@/views/login'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
