1f25be9829cef5672d56c0ff46d91b0bc34fa069
lily <905355741@qq.com>
2022-09-06 16:13:27 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    您好
+    <span class="toutiao toutiao-gengduo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">登陆</div>
   </div>
 </template>
 
 <script>
+// rem 适配
+// 1、 yarn add amfe-flexible
+// 2、盒子使用rem单位
+// 3、自动识别css中的px，并处理rem
+//    post-css 预处理器
+//    yarn add -D postcss-pxtorem  （供post-css使用,建立.postcssrc.js文件夹）
 export default {
 
 }
 </script>
 
-<style>
-
+<style scoped>
+.box {
+  width: 2rem;
+  height: 2rem;
+  background-color: aquamarine;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/fonts/iconfont.css'
 
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入flexible
+import 'amfe-flexible'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
